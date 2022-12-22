bc1f3eed3849c556b65732f45dcaaa1ee847a050
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2022-12-22 11:52:20 -0500

    updates

--- components/ThemeToggler/index.js
+++ components/ThemeToggler/index.js
@@ -3,12 +3,13 @@ import { useTheme } from "next-themes";
 
 const ThemeToggler = () => {
   const { theme, setTheme } = useTheme();
-  const [timeRemaining, setTimeRemaining] = useState("00:00");
+  const [timeRemaining, setTimeRemaining] = useState("00:00:00");
 
   useEffect(() => {
     const currentTime = new Date();
     const currentHour = currentTime.getHours();
     const currentMinute = currentTime.getMinutes();
+    const currentSecond = currentTime.getSeconds();
 
     let intervalId;
 
@@ -17,34 +18,46 @@ const ThemeToggler = () => {
       setTheme("dark");
       const hoursRemaining = (9 - currentHour + 24) % 24;
       const minutesRemaining = 60 - currentMinute;
+      const secondsRemaining = 60 - currentSecond;
       setTimeRemaining(
         `${hoursRemaining < 10 ? "0" + hoursRemaining : hoursRemaining}:${
           minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining
+        }:${
+          secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining
         }`
       );
       intervalId = setInterval(() => {
         setTimeRemaining((prevTime) => {
-          const [hours, minutes] = prevTime.split(":").map(Number);
-          if (minutes === 0 && hours === 0) {
+          const [hours, minutes, seconds] = prevTime.split(":").map(Number);
+          if (minutes === 0 && hours === 0 && seconds === 0) {
             clearInterval(intervalId);
             setTheme("light");
-            return "00:00";
+            return "00:00:00";
           }
-          if (minutes === 0) {
-            return `${
-              hours === 1
+          if (seconds === 0) {
+            if (minutes === 0) {
+              return `${
+                hours === 1
+                  ? "00"
+                  : hours - 1 < 10
+                  ? "0" + (hours - 1)
+                  : hours - 1
+              }:59:59`;
+            }
+            return `${hours}:${
+              minutes === 1
                 ? "00"
-                : hours - 1 < 10
-                ? "0" + (hours - 1)
-                : hours - 1
+                : minutes - 1 < 10
+                ? "0" + (minutes - 1)
+                : minutes - 1
             }:59`;
           }
-          return `${hours}:${
-            minutes === 1
+          return `${hours}:${minutes}:${
+            seconds === 1
               ? "00"
-              : minutes - 1 < 10
-              ? "0" + (minutes - 1)
-              : minutes - 1
+              : seconds - 1 < 10
+              ? "0" + (seconds - 1)
+              : seconds - 1
           }`;
         });
       }, 1000);
@@ -53,39 +66,51 @@ const ThemeToggler = () => {
       setTheme("light");
       const hoursRemaining = (21 - currentHour + 24) % 24;
       const minutesRemaining = 60 - currentMinute;
+      const secondsRemaining = 60 - currentSecond;
       setTimeRemaining(
         `${hoursRemaining < 10 ? "0" + hoursRemaining : hoursRemaining}:${
           minutesRemaining < 10 ? "0" + minutesRemaining : minutesRemaining
+        }:${
+          secondsRemaining < 10 ? "0" + secondsRemaining : secondsRemaining
         }`
       );
       intervalId = setInterval(() => {
         setTimeRemaining((prevTime) => {
-          const [hours, minutes] = prevTime.split(":").map(Number);
-          if (minutes === 0 && hours === 0) {
+          const [
+hours, minutes, seconds] = prevTime.split(":").map(Number);
+          if (minutes === 0 && hours === 0 && seconds === 0) {
             clearInterval(intervalId);
             setTheme("dark");
-            return "00:00";
+            return "00:00:00";
           }
-          if (minutes === 0) {
-            return `${
-              hours === 1
+          if (seconds === 0) {
+            if (minutes === 0) {
+              return `${
+                hours === 1
+                  ? "00"
+                  : hours - 1 < 10
+                  ? "0" + (hours - 1)
+                  : hours - 1
+              }:59:59`;
+            }
+            return `${hours}:${
+              minutes === 1
                 ? "00"
-                : hours - 1 < 10
-                ? "0" + (hours - 1)
-                : hours - 1
+                : minutes - 1 < 10
+                ? "0" + (minutes - 1)
+                : minutes - 1
             }:59`;
           }
-          return `${hours}:${
-            minutes === 1
+          return `${hours}:${minutes}:${
+            seconds === 1
               ? "00"
-              : minutes - 1 < 10
-              ? "0" + (minutes - 1)
-              : minutes - 1
+              : seconds - 1 < 10
+              ? "0" + (seconds - 1)
+              : seconds - 1
           }`;
         });
       }, 1000);
     }
-
     return () => clearInterval(intervalId);
   }, []);
 
