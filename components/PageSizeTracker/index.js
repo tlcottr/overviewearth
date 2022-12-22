2433eb0c3939f41fe0413b7e3d3160f69c5eccc4
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2022-12-22 01:34:45 -0500

    HARDWARE CONCURRENCY

--- components/PageSizeTracker/index.js
+++ components/PageSizeTracker/index.js
@@ -5,24 +5,16 @@ const PageSizeTracker = () => {
 
   useEffect(() => {
     const trackPageSize = () => {
-      var resources = window.performance.getEntriesByType("resource");
-      var totalSize = 0;
-      for (var i = 0; i < resources.length; i++) {
-        totalSize += resources[i].transferSize;
-      }
-      var sizeInKB = (totalSize / 1024).toFixed(2);
-
-      // Check if page size has already been stored in localStorage
-      const storedPageSize = localStorage.getItem("pageSize");
-      if (!storedPageSize) {
-        // If not, store the current page size in localStorage
-        localStorage.setItem("pageSize", sizeInKB);
+      if (window.navigator && window.navigator.hardwareConcurrency) {
+        // The navigator.hardwareConcurrency property is supported
+        // in this browser
+        const hardwareConcurrency = window.navigator.hardwareConcurrency;
+        const pageSizeInKB = Math.round(hardwareConcurrency * 1024);
+        setPageSize(pageSizeInKB);
       } else {
-        // If it has, use the stored value as the initial value for pageSize
-        // if it is different from the current page size
-        if (storedPageSize !== sizeInKB) {
-          setPageSize(storedPageSize);
-        }
+        // The navigator.hardwareConcurrency property is not supported
+        // in this browser
+        setPageSize("N/A");
       }
     };
 
