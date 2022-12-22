1cc8831c177ef677fb811520fa53d9232bf0c332
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2022-12-22 00:08:29 -0500

    OG title

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 export const constants = {
   pageTitle: "Overview.Earth",
-  pageDescription: "Mint any Wikipedia entry as an NFT",
+  pageDescription:
+    "an early stage investment vehicle for companies specializing in climate mitigation and environmental regeneration.",
 };
