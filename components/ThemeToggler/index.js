0f05bbfe762f01307ea8ee0410923608d8587210
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-08 18:50:58 -0500

    HUD updates > show correct time

--- components/ThemeToggler/index.js
+++ components/ThemeToggler/index.js
@@ -8,54 +8,41 @@ const ThemeToggler = () => {
   const currentMinute = currentTime.getMinutes();
   const currentSecond = currentTime.getSeconds();
 
+  let hoursUntilNextDay = 0;
+  if (currentHour < 1) {
+    hoursUntilNextDay = 1 - currentHour;
+  } else {
+    hoursUntilNextDay = 24 - currentHour;
+  }
+
   useEffect(() => {
     let intervalId;
     setTimeRemaining(
-      `${(1 - currentHour + 24) % 24}:${60 - currentMinute}:${
-        60 - currentSecond
-      }`
+      `${hoursUntilNextDay}:${60 - currentMinute}:${60 - currentSecond}`
     );
     intervalId = setInterval(() => {
       setTimeRemaining((prevTime) => {
         const [hours, minutes, seconds] = prevTime.split(":").map(Number);
-        if (minutes === 0 && hours === 0 && seconds === 0) {
+        if (hours === 0 && minutes === 0 && seconds === 0) {
           clearInterval(intervalId);
           return "00:00:00";
         }
         if (seconds === 0) {
           if (minutes === 0) {
-            return `${
-              hours === 1
-                ? "00"
-                : hours - 1 < 10
-                ? "0" + (hours - 1)
-                : hours - 1
-            }:59:59`;
+            return `${hours - 1 < 10 ? "0" + (hours - 1) : hours - 1}:59:59`;
           }
           return `${hours}:${
-            minutes === 1
-              ? "00"
-              : minutes - 1 < 10
-              ? "0" + (minutes - 1)
-              : minutes - 1
+            minutes - 1 < 10 ? "0" + (minutes - 1) : minutes - 1
           }:59`;
         }
         return `${hours}:${minutes}:${
-          seconds === 1
-            ? "00"
-            : seconds - 1 < 10
-            ? "0" + (seconds - 1)
-            : seconds - 1
+          seconds - 1 < 10 ? "0" + (seconds - 1) : seconds - 1
         }`;
       });
     }, 1000);
     return () => clearInterval(intervalId);
   }, []);
 
-  return (
-    <div className>
-      Offline Mode in {timeRemaining}
-    </div>
-  );
+  return <div className>Offline Mode in {timeRemaining}</div>;
 };
 export default ThemeToggler;
