3022cc02fd5178aebee7a876f813ae9990e26966
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-08 21:59:41 -0500

    added separate layout for offline mode

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,6 +3,7 @@ import Header from "../Header";
 import Head from "next/head";
 import { constants } from "../../constants";
 import Footer from "../Footer";
+import LogoSm from "../LogoSm";
 
 const currentHour = new Date().getHours();
 const Online =
@@ -35,8 +36,17 @@ const OfflineLayout = ({ children }) => {
         <meta name="description" content={constants.pageDescription} />*/}
         <meta name="viewport" content="width=device-width" />
       </Head>
-      <div className="h-screen p-4 md:p-8">
-        <main className="">{children}</main>
+      <div className="h-screen p-4 md:p-8 flex justify-center items-center">
+        <main className="flex flex-col md:flex-row justify-between items-start max-w-full">
+          <div className="mr-[0] md:mr-[100px] mb-5 md:mb-[0] pt-[0] md:pt-[12.5px]">
+            <LogoSm />
+          </div>
+          <div className="max-w-2xl font-helvetica font-normal text-4xl lowercase">
+            We are Overview Capital, an early stage investment vehicle for
+            companies specializing in climate mitigation and environmental
+            regeneration.
+          </div>
+        </main>
       </div>
     </>
   );
