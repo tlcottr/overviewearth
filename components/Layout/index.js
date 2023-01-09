d04ab633889aaf2b0aa32da6767bd7226dcd44b0
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-08 19:32:08 -0500

    updated Layout.js to hold conditional operation based on time of day.

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,7 +4,13 @@ import Head from "next/head";
 import { constants } from "../../constants";
 import Footer from "../Footer";
 
-const Layout = ({ children }) => {
+const currentHour = new Date().getHours();
+const Online =
+  (currentHour >= 8 && currentHour < 24) ||
+  (currentHour >= 0 && currentHour < 1);
+console.log(Online);
+
+const OnlineLayout = ({ children }) => {
   return (
     <>
       <Head>
@@ -19,6 +25,25 @@ const Layout = ({ children }) => {
       </div>
     </>
   );
+};
+
+const OfflineLayout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        {/*<meta name="title" content={constants.pageTitle} />
+        <meta name="description" content={constants.pageDescription} />*/}
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <div className="h-screen p-4 md:p-8">
+        <main className="">{children}</main>
+      </div>
+    </>
+  );
+};
+
+const Layout = () => {
+  return Online ? <OnlineLayout /> : <OfflineLayout />;
 };
 
 export default Layout;
