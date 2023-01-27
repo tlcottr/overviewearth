b914e16103c2534f74df5abefdb85d910e3d905b
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-27 01:33:32 -0500

    mobile layout update

--- components/LandingLayout/index.js
+++ components/LandingLayout/index.js
@@ -78,27 +78,28 @@ const LandingLayout = ({ children }) => {
           <meta name="viewport" content="width=device-width" />
         </Head>
         <div className="h-screen p-4 md:p-8 flex flex-col items-center justify-center">
-          <div className="h-screen md:h-auto flex flex-col md:flex-row items-center md:items-start justify-between font-inconsolata max-w-5xl m-2">
-            <div className="pt-1">
+          <div className="flex flex-col items-center justify-center font-inconsolata max-w-5xl">
+            <div className="pt-1 absolute top-2">
               <Logo />
             </div>
             <div className="text-center md:text-left text-xl max-w-sm leading-tight">
               Overview is a venture capital firm investing in climate change
               solutions.
             </div>
-
-            <div className="mt-11 py-2 h-3 flex flex-row items-center">
-              <div
-                className="text-xs max-w-sm leading-tight uppercase cursor-pointer"
-                onClick={copyToClipboard}
-              >
-                contact@overview.earth
-              </div>
-              {showTooltip && (
-                <div className="ml-2 tooltip font-inconsolata text-xs bg-[#1A1A1A] text-[#DDD7C9] rounded-md w-[125px] px-1 text-center">
-                  Copied to clipboard
+            <div className="absolute bottom-4">
+              <div className="flex flex-row items-center">
+                <div
+                  className="text-xs max-w-sm leading-tight uppercase cursor-pointer"
+                  onClick={copyToClipboard}
+                >
+                  contact@overview.earth
                 </div>
-              )}
+                {showTooltip && (
+                  <div className="ml-2 tooltip font-inconsolata text-xs bg-[#1A1A1A] text-[#DDD7C9] rounded-md w-[125px] px-1 text-center">
+                    Copied to clipboard
+                  </div>
+                )}
+              </div>
             </div>
           </div>
         </div>
