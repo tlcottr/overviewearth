384e9d39163aab05db6aecf4881658059389bbad
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-04-26 12:32:59 -0400

    update

--- components/LandingLayout/index.js
+++ components/LandingLayout/index.js
@@ -343,8 +343,8 @@ const LandingLayout = ({ children }) => {
         <meta name="description" content={constants.pageDescription} />*/}
           <meta name="viewport" content="width=device-width" />
         </Head>
-        <div className="flex flex-col items-center justify-center">
-          <div className="h-auto min-h-screen flex flex-col items-center justify-center font-inconsolata max-w-5xl">
+        <div className="min-h-screen flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center font-inconsolata max-w-5xl">
             <div className="pt-1 mb-8">
               <Logo />
             </div>
