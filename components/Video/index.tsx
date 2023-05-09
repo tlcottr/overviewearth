33f487afc8a7ba5606b231edc100b4136a7c2adb
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-05-09 16:57:34 -0400

    update mov positioning

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -30,6 +30,8 @@ const Video: React.FC<Props> = (props: Props) => {
         position: "fixed",
         top: 0,
         left: 0,
+        bottom: 0,
+        right: 0,
         width: "100%",
         height: "100%",
         overflow: "hidden",
