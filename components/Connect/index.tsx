9ae37e6ceeb82a1e01d64e0cd910e9d19c088d44
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-06-02 14:50:57 -0400

    updat

--- components/Connect/index.tsx
+++ components/Connect/index.tsx
@@ -36,62 +36,57 @@ const Contact = (props: Props) => {
   };
 
   const bind = useDrag(({ swipe: [swipeY] }) => {
-    isMobile && {};
-    if (swipeY >= 0) {
+    if (swipeY >= .1 && isMobile) {
       handleSwipeDown();
     }
   });
   return (
     <>
-      <div {...bind()}>
-        <div className={styles.arrowContainer}>
-          <Return size={50} />
-        </div>
-        <div className={styles.container}>
-          <div className={styles.top}>
-            <span className={styles.border}></span>
-            <div className={styles.center}>
-              <div onClick={handleCopy} className={styles.email}>
-                {isCopied ? (
-                  <span className={styles.copied}>COPIED TO CLIPBOARD</span>
-                ) : (
-                  <span>
-                    contact<span className={styles.emailSymbol}>@</span>
-                    overview.earth
-                  </span>
-                )}
-              </div>
-              <div className={styles.socialIcons}>
-                <Link
-                  href={"https://www.instagram.com/overview.capital/"}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className={styles.icon}>
-                    <InstagramIcon />
-                  </span>
-                </Link>
-                <Link
-                  href={
-                    "https://www.linkedin.com/company/overviewcapital/about/"
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className={styles.icon}>
-                    <LinkedInIcon />
-                  </span>
-                </Link>
-                <Link
-                  href={"https://twitter.com/overviewcap"}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className={styles.icon}>
-                    <TwitterIcon />
-                  </span>
-                </Link>
-              </div>
+      <div className={styles.arrowContainer}>
+        <Return size={50} />
+      </div>
+      <div className={styles.container} {...bind()}>
+        <div className={styles.top}>
+          <span className={styles.border}></span>
+          <div className={styles.center}>
+            <div onClick={handleCopy} className={styles.email}>
+              {isCopied ? (
+                <span className={styles.copied}>COPIED TO CLIPBOARD</span>
+              ) : (
+                <span>
+                  contact<span className={styles.emailSymbol}>@</span>
+                  overview.earth
+                </span>
+              )}
+            </div>
+            <div className={styles.socialIcons}>
+              <Link
+                href={"https://www.instagram.com/overview.capital/"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={styles.icon}>
+                  <InstagramIcon />
+                </span>
+              </Link>
+              <Link
+                href={"https://www.linkedin.com/company/overviewcapital/about/"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={styles.icon}>
+                  <LinkedInIcon />
+                </span>
+              </Link>
+              <Link
+                href={"https://twitter.com/overviewcap"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <span className={styles.icon}>
+                  <TwitterIcon />
+                </span>
+              </Link>
             </div>
           </div>
         </div>
