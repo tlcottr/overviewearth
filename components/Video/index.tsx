d9d25c5e1df5a671cec7aa1f9a204d09e6efd510
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-06-16 10:19:16 -0400

    update

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -78,7 +78,7 @@ const Video: React.FC<Props> = (props: Props) => {
           ...(showTeam && {
             transform: "translate(-50%, -50%) translateY(50%)",
             ...(isMobile && {
-              transform: "translate(-50%, -50%) translateY(30%)",
+              transform: "translate(-50%, -50%) translateY(40%)",
             }),
           }),
           ...(showContact && {
